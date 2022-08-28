09a3829d9ee484eabf0d7cf37e80e33892fc4730
Shelly Geng <15002157293@163.com>
2022-08-28 11:30:17 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,15 @@ import { createRouter, createWebHistory} from "vue-router"
 const routes= [
     {
         path: '/',
-        component: () => import('@/pages/Login.vue')
+        component: () => import('pages/Login.vue')
     },
     {
         path: '/login',
-        component: () => import('@/pages/Login.vue')
+        component: () => import('pages/Login.vue')
     },
     {
         path: '/register',
-        component: () => import('@/pages/Register.vue')
+        component: () => import('pages/Register.vue')
     }
 ]
 
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import pluginLegacy from "@vitejs/plugin-legacy"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      'pages': '@/pages',
-      'assets': '@/assets',
-      'common': '@/common',
-      'components': '@components',
-      'network': '@/network',
-      'router': '@/router',
-      'styles': '@/styles'
-    },
-    extensions: ['', '.js', '.json', '.vue', '.scss', '.css']
-  },
-  css: {
-    // css预处理器
-    preprocessorOptions: {
-      less: {
-        charset: false,
-        // additionalData: '@import "./src/assets/style/global.less";',
+const resolvePath = function (str) {
+  let pathStr = str ? ('src/' + str) : 'src'
+  return path.resolve(__dirname, pathStr);
+}
+export default defineConfig((command, mode) => {
+  console.log('command, mode', command, mode);
+  return {
+    plugins: [
+      vue(),
+      pluginLegacy({
+        targets: ['defaults', 'not ie 11']
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': resolvePath(),
+        'pages': resolvePath('pages'),
+        'assets': resolvePath('assets'),
+        'common': resolvePath('common'),
+        'components': resolvePath('components'),
+        'network': resolvePath('network'),
+        'router': resolvePath('router'),
+        'styles': resolvePath('styles')
       },
+      extensions: ['', '.js', '.ts', '.json', '.vue', '.scss', '.css']
     },
+    css: {
+      // css预处理器
+      preprocessorOptions: {
+        less: {
+          charset: false,
+          // additionalData: '@import "./src/assets/style/global.less";',
+        },
+      },
+    }
   }
+
 })
